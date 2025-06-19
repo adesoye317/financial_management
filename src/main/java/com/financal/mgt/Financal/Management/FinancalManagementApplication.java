d54c98a08604f8f634f6481ab3dba5b18a3e864e
aadesoye <aadesoye@ecobank.com>
2025-06-19 13:47:04 +0100

    creating mongo db config for financial management

---
+++ src/main/java/com/financal/mgt/Financal/Management/FinancalManagementApplication.java
@@ -0,0 +1,13 @@
+package com.financal.mgt.Financal.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancalManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancalManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/financal/mgt/Financal/Management/FinancalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.financal.mgt.Financal.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
