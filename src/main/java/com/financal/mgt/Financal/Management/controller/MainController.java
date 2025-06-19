d46d0138324e6e04b181662c351aa804fa50387a
aadesoye <aadesoye@ecobank.com>
2025-06-19 15:04:58 +0100

    add customer endpoint

--- src/main/java/com/financal/mgt/Financal/Management/controller/MainController.java
+++ src/main/java/com/financal/mgt/Financal/Management/controller/MainController.java
@@ -1,7 +1,29 @@
 package com.financal.mgt.Financal.Management.controller;
 
+import com.financal.mgt.Financal.Management.dto.request.CustomerSignUpRequest;
+import com.financal.mgt.Financal.Management.service.CustomerService;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/customers")
 public class MainController {
+
+
+    private final CustomerService customerService;
+
+
+    public MainController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+
+    @PostMapping("/signup")
+    public Object signUp(@Valid @RequestBody CustomerSignUpRequest request) {
+        return customerService.signUp(request);
+    }
+
 }
